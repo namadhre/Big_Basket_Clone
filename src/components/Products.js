b4fa1e3a0b04b2f64b9355f88b68cb10859e4ae0
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-02-01 19:35:47 +0530

    Modified: Products.js Retrieved the data from the redux store and rendered the data on the web-page.

---
+++ src/components/Products.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import "./Products.css"
+
+class Products extends Component {
+    render() {
+        return this.props.items.map((product) => (
+            <div className="col-sm-3 item-container" key={product.id}>
+                <img className='img-thumbnail' src={product.image} />
+                <p>{product.brand}</p>
+                <h1>{product.name}</h1>
+                <div className="d-flex flex-column mb-5 dropdown-wrapper">
+                    <select className="select form-control-lg drop-down price-container">
+                        {product.price.map((itemPrice, index) => {
+                            return <option className="option-container" value={index+1} key={itemPrice.quantity}>{`${itemPrice.quantity} kg - ₹ ${itemPrice.price}`}</option>
+                        })}
+                    </select>
+                </div>
+
+            </div>
+        ));
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.itemsData.items
+    }
+}
+
+export default connect(mapStateToProps)(Products);
+
